01efead624db100355c294a6c47a2fcef07de659
Wessel Frijters <wessel.frijters@gmail.com>
2018-01-08 11:56:01 +0100

    TEST

---
+++ Assets/Scripts/UnitData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitData : ScriptableObject
+{
+    public float Health;
+    public float Damage;
+    public float Speed;
+    public float AttackSpeed;
+    public float Range;
+    public float Cost;
+
+}
---
+++ Assets/Scripts/Units/GenericUnit.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Team
+{
+Team1,
+Team2
+}
+
+public enum WalkDireciton
+{
+Left,
+Right
+}
+
+public class GenericUnit : MonoBehaviour {
+
+    [SerializeField]
+    private Team m_Team;
+
+    private UnitData m_Data;
+
+    private float m_Health;
+    private float m_Speed;
+    private float m_Range;
+    private float m_AttackSpeed;
+
+    public void Initialize(Team team, UnitData data)
+    {
+        m_Health = data.Health;
+        m_Speed = data.Speed;
+        m_Range = data.Range;
+        m_AttackSpeed = data.AttackSpeed;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
