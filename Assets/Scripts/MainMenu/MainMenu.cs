e2b614d5026b5ad62382cb181cae4b8ab09e8238
Wessel Frijters <wessel.frijters@gmail.com>
2018-01-12 17:28:37 +0100

    TEST

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button m_Start;
+    [SerializeField] private Button m_Quit;
+    [SerializeField] private GameObject m_AreYouSure;
+    [SerializeField] private Button m_Yes, m_No;
+
+    private void Start()
+    {
+        Button start = m_Start.GetComponent<Button>();
+        start.onClick.AddListener(StartClicked);
+
+        Button quit = m_Quit.GetComponent<Button>();
+        quit.onClick.AddListener(QuitClicked);
+
+        Button yes = m_Yes.GetComponent<Button>();
+        yes.onClick.AddListener(YesClicked);
+
+        Button no = m_No.GetComponent<Button>();
+        no.onClick.AddListener(NoClicked);
+    }
+
+    private void StartClicked()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    private void QuitClicked()
+    {
+        m_AreYouSure.SetActive(true);
+        m_Start.gameObject.SetActive(false);
+        m_Quit.gameObject.SetActive(false);
+    }
+
+    private void YesClicked()
+    {
+        Application.Quit();
+    }
+
+    private void NoClicked()
+    {
+        m_AreYouSure.SetActive(false);
+        m_Start.gameObject.SetActive(true);
+        m_Quit.gameObject.SetActive(true);
+    }
+}
