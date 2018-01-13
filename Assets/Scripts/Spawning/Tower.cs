4b95816a93536330b7ec1a8014ffc96149559ea6
Robin <521193@student.glu.nl>
2018-01-13 12:45:34 +0100

    towers and uibuttonsart

--- Assets/Scripts/Spawning/Tower.cs
+++ Assets/Scripts/Spawning/Tower.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    private UnitData m_UnitData;
+    private GameObject m_Arrow;
 
+    private float m_Shooting;
+    private float m_NextShot;
+
+    public void Initialize(UnitData UD)
+    {
+        m_UnitData = UD;
+        m_Shooting = m_UnitData.AttackSpeed;
+        m_Arrow = Resources.Load("Arrow") as GameObject;
+        this.gameObject.AddComponent<Bow>();
+        this.gameObject.GetComponent<Bow>().Initialize(GameObject.Find("ShootingRange").transform,GameObject.Find("TowerPool").GetComponent<ObjectPool>().GetObject().transform);
+    }
+
+    private void Update()
+    {
+        if(Time.time > m_NextShot)
+        {
+            m_NextShot = Time.time + m_Shooting;
+            this.GetComponent<Bow>().SimulateProjectile();
+        }
+    }
 }
