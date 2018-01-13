c39088b74757a6df331ad02cbff4cba58cb8e390
wesselfr <wessel.frijters@gmail.com>
2018-01-13 11:58:35 +0100

    Update Switch.cs

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -23,6 +23,17 @@ public class Switch : Waypoints {
             m_RightTarget = m_SwitchPositions[m_Current];
         }
     }
+    
+    public void SwitchLane()		
+     {		
+         m_Current++;		
+         if(m_Current >= m_SwitchPositions.Length)		
+         {		
+             m_Current = 0;		
+         }		
+ 		
+         GetTarget(m_SwitchDireciton);		
+     }
 
     public override Vector3 GetTarget(WalkDireciton direction)
     {
