ff827d64c575ac69cdb04c933b0c2355ffc9f0a3
Robin <521193@student.glu.nl>
2018-01-13 12:00:56 +0100

    committer

---
+++ Assets/Scripts/Spawning/TowerUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TowerUI : MonoBehaviour
+{
+    private Sprite m_Image;
+    private void Start()
+    {
+            
+    }
+
+    private void Update()
+    {
+        
+    }
+}
