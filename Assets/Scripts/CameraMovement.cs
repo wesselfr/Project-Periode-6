c2d253208fb40732122325700b9c1e97c78a4281
Wessel Frijters <wessel.frijters@gmail.com>
2018-01-13 16:35:42 +0100

    Smooth Camera

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float m_Speed;
+
+    [SerializeField]
+    private float m_MinX, m_MaxX;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += (Vector3.right * Input.GetAxis("Horizontal") * m_Speed) * Time.deltaTime; ;
+
+        if(transform.position.x < m_MinX || transform.position.x > m_MaxX)
+        {
+            if(transform.position.x < m_MinX)
+            {
+                transform.position = transform.position + Vector3.right * 0.1f * ((m_MaxX - transform.position.x) / 10);
+            }
+            else if(transform.position.x > m_MaxX)
+            {
+                transform.position = transform.position + Vector3.right * 0.1f * ((m_MaxX - transform.position.x) / 10);
+            }
+        }
+        
+	}
+}
