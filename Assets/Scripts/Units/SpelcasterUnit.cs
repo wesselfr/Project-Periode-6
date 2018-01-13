4ce680c724e2079e522ee8dc886a075410de10bb
Wessel Frijters <wessel.frijters@gmail.com>
2018-01-13 12:22:58 +0100

    Spelcaster

---
+++ Assets/Scripts/Units/SpelcasterUnit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpelcasterUnit : GenericUnit {
+
+    public override void AttackUnit(GenericUnit unit)
+    {
+        base.AttackUnit(unit);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Found Unit
+        if (other.tag.Contains("Unit"))
+        {
+            GenericUnit unit = other.GetComponent<GenericUnit>();
+            if (unit.GetTeam() == m_Team)
+            {
+                m_FocussedUnit = unit;
+            }
+        }
+
+        if (other.tag.Contains("Waypoint"))
+        {
+            m_Target = (other.GetComponent<Waypoints>().GetTarget(m_Direciton) - transform.position).normalized;
+        }
+    }
+}
